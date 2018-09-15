3ff3214b3e68c4583c25207ff3a7ef01c1d3cba6
Alvindu13 <alcaraz.jeremie@hotmail.fr>
2018-09-15 21:44:03 +0200

    Ajout d'une fonction pour calculer un nombre aléatoire à nB chiffres

--- Partie_1/src/com/company/Game.java
+++ Partie_1/src/com/company/Game.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 
 public class Game {
 
+    /**
+     * Affiche texte choix de la longueur du chiffre à trouver
+     */
     public void numberRun() {
-        System.out.println("Veuillez proposer nombre de chiffres pour la combinaison à rechercher (max 5) : ");
+        System.out.println("Veuillez proposer un nombre entre 1 et 10 pour déterminer le nombre de chiffre de la combinaison à trouver : ");
         Scanner sc = new Scanner(System.in);
         int nb = sc.nextInt();
         this.displayCombinaison(nb);
+        this.aleatoireNumber(nb);
     }
 
+    /**
+     * Choix du chiffre pour la longueur de la combi
+     * @param nC
+     */
 
     public void displayCombinaison(int nC) {
         if (nC >= 1) {
@@ -19,7 +27,20 @@ public class Game {
             System.out.println("Merci de mettre un chiffre valide\n");
             numberRun();
         }
-
     }
-    
+
+    /**
+     * Génère un nombre aléatoire de nB chiffre(s)
+     * @param nB
+     */
+
+    public void aleatoireNumber(int nB) {
+        int bMin=(int)Math.pow(10,nB-1);
+        int bMax=(int)Math.pow(10,nB);
+        int nombre = (int)(Math.random() * (bMax - bMin)) + bMin ;
+        System.out.println("Le nombre est : " + nombre);
+        }
+
 }
+
+
