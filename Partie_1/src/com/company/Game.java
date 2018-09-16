11616abb21f0200e8529007734c53b4f2b04bd41
Alvindu13 <alcaraz.jeremie@hotmail.fr>
2018-09-16 17:07:54 +0200

    Ajout d'une boucle pour rejouer si on le souhaite.
    Toutefois :
    - Défaut si on entre un nombre avec moins de chiffres que souhaité
    - Défaut si caractères spéciaux

--- Partie_1/src/com/company/Game.java
+++ Partie_1/src/com/company/Game.java
@@ -12,7 +12,9 @@ public class Game {
         Scanner sc = new Scanner(System.in);
         int nb = sc.nextInt();
         this.availableCommand(nb);
-        this.aleatoireNumberAndSelectedNumber(nb);
+        int n = aleatoireNumberAndSelectedNumber(nb);
+        displayPropose(n);
+        retry();
         // Trouver une façon de ne pas prendre compte des caractères autres que les nombres entier
     }
 
@@ -40,6 +42,7 @@ public class Game {
                 break;
             default:
                 System.out.println("Je n'ai pas compris votre réponse. Merci de mettre un chiffre valide\n");
+                numberRun();
                 break;
         }
 
@@ -64,11 +67,12 @@ public class Game {
      * @param nB
      */
 
-    public void aleatoireNumberAndSelectedNumber(int nB) {
+    public int aleatoireNumberAndSelectedNumber(int nB) {
         int bMin=(int)Math.pow(10,nB-1);
         int bMax=(int)Math.pow(10,nB);
         int nombre = (int)(Math.random() * (bMax - bMin)) + bMin ;
-        this.displayPropose(nombre);
+        return nombre;
+        //this.displayPropose(nombre);
     }
 
 
@@ -80,28 +84,26 @@ public class Game {
         System.out.println("Vous avez le droit à 5 tentatives");
         System.out.print("Merci faire votre proposition : ");
         do {
-
-            Scanner sc = new Scanner (System.in);
-                System.out.println();
-                System.out.println("Il vous reste encore " + essai + " tentatives");
-                m = sc.nextInt();
-                String F = str+nB;
-                String G = str+m;
-                //Integer.toString(m);
-                //System.out.println("Combinaison secrète : " + nB);
-                System.out.print("Votre proposition : " + m + " -> réponse : " );
-                for(i = 0; i < F.length(); i++){
-                    if (G.charAt(i) == F.charAt(i)){
-                        System.out.print("=");
-                    } else if (G.charAt(i) < F.charAt(i)){
-                        System.out.print("+");
-                    } else if (G.charAt(i) > F.charAt(i)){
-                        System.out.print("-");
-                    }
-
+            Scanner sc = new Scanner(System.in);
+            System.out.println();
+            System.out.println("Il vous reste encore " + essai + " tentatives");
+            m = sc.nextInt();
+            String F = str + nB;
+            String G = str + m;
+            //Integer.toString(m);
+            //System.out.println("Combinaison secrète : " + nB);
+            System.out.print("Votre proposition : " + m + " -> réponse : ");
+            for (i = 0; i < F.length(); i++) {
+                if (G.charAt(i) == F.charAt(i)) {
+                    System.out.print("=");
+                } else if (G.charAt(i) < F.charAt(i)) {
+                    System.out.print("+");
+                } else if (G.charAt(i) > F.charAt(i)) {
+                    System.out.print("-");
                 }
-                essai--;
 
+            }
+            essai--;
 
 
         } while (m != nB && essai != 0);
@@ -109,12 +111,24 @@ public class Game {
             System.out.println();
             System.out.println("Bravo ! La combinaison secrète était bien " + nB);
         } else {
+            System.out.println();
             System.out.println("Malheureusement vous n'avez pas trouvé la bonne combinaison qui était :  "  + nB);
 
         }
 
     }
 
+    public void retry(){
+
+        System.out.println("Voulez-vous rejouer ? Si oui, veuillez entrer OK");
+        Scanner sc = new Scanner (System.in);
+        String str = sc.nextLine();
+        while (str.contains("OK")){
+            numberRun();
+        }
+            System.out.println("Je vous remercie d'avoir joué. À bientôt ! ");
+    }
 }
+
 
 
