ff262d9de7d2607d88e68b805dd45d8410b8da44
Alvindu13 <alcaraz.jeremie@hotmail.fr>
2018-09-16 16:14:59 +0200

    Code allégé, nouvelle version fonctionnelle.
    Toutefois :
    - Défaut si on entre un nombre avec moins de chiffres que souhaité
    - Défaut si caractères spéciaux

--- Partie_1/src/com/company/Game.java
+++ Partie_1/src/com/company/Game.java
@@ -8,11 +8,11 @@ public class Game {
      * Affiche texte choix de la longueur du chiffre à trouver
      */
     public void numberRun() {
-        System.out.println("Veuillez proposer un nombre entre 1 et 10 pour déterminer le nombre de chiffre de la combinaison à trouver : ");
+        System.out.println("Veuillez proposer un nombre entre 1 et 5 pour déterminer le nombre de chiffre de la combinaison à trouver : ");
         Scanner sc = new Scanner(System.in);
         int nb = sc.nextInt();
-        this.displayCombinaison(nb);
-        this.aleatoireNumber(nb);
+        this.availableCommand(nb);
+        this.aleatoireNumberAndSelectedNumber(nb);
         // Trouver une façon de ne pas prendre compte des caractères autres que les nombres entier
     }
 
@@ -21,74 +21,98 @@ public class Game {
      * @param nb
      * @return
      */
-    /*public boolean availableCommand(int nb){
-        bollean
-        if (nb >= 1 && nb <=10) {
-            System.out.println("Vous avez choisi un nombre à " + nC + " chiffres");
-        } else {
-            System.out.println("Je n'ai pas compris votre réponse. Merci de mettre un chiffre valide\n");
-            numberRun();
+    public void availableCommand(int nb){
+        switch (nb) {
+            case 1:
+                System.out.println("Vous avez choisi un nombre à " + nb + " chiffres");
+                break;
+            case 2:
+                System.out.println("Vous avez choisi un nombre à " + nb + " chiffres");
+                break;
+            case 3:
+                System.out.println("Vous avez choisi un nombre à " + nb + " chiffres");
+                break;
+            case 4:
+                System.out.println("Vous avez choisi un nombre à " + nb + " chiffres");
+                break;
+            case 5:
+                System.out.println("Vous avez choisi un nombre à " + nb + " chiffres");
+                break;
+            default:
+                System.out.println("Je n'ai pas compris votre réponse. Merci de mettre un chiffre valide\n");
+                break;
         }
 
-    }*/
+    }
 
     /**
      * Choix du chiffre pour la longueur de la combi
      * @param nC
      */
 
-    public void displayCombinaison(int nC) {
+    /*public void displayCombinaison(int nC) {
         if (nC >= 1 && nC <=10) {
             System.out.println("Vous avez choisi un nombre à " + nC + " chiffres");
         } else {
             System.out.println("Je n'ai pas compris votre réponse. Merci de mettre un chiffre valide\n");
             numberRun();
         }
-    }
+    }*/
 
     /**
      * Génère un nombre aléatoire de nB chiffre(s)
      * @param nB
      */
 
-    public void aleatoireNumber(int nB) {
+    public void aleatoireNumberAndSelectedNumber(int nB) {
         int bMin=(int)Math.pow(10,nB-1);
         int bMax=(int)Math.pow(10,nB);
         int nombre = (int)(Math.random() * (bMax - bMin)) + bMin ;
-        //System.out.println("Le nombre est : " + nombre);
         this.displayPropose(nombre);
+    }
 
-        }
 
     public void displayPropose(int nB) {
         int m;
         int i;
+        int essai = 5;
         String str = "";
+        System.out.println("Vous avez le droit à 5 tentatives");
         System.out.print("Merci faire votre proposition : ");
-        do{
+        do {
+
             Scanner sc = new Scanner (System.in);
-            System.out.println();
-            m = sc.nextInt();
-            String F = str+nB;
-            String G = str+m;
-            //Integer.toString(m);
-            //System.out.println("Combinaison secrète : " + nB);
-            System.out.print("Votre proposition : " + m + " -> réponse : " );
-            for(i = 0; i < F.length(); i++){
-                if (G.charAt(i) == F.charAt(i)){
-                    System.out.print("=");
-                } else if (G.charAt(i) < F.charAt(i)){
-                    System.out.print("+");
-                } else if (G.charAt(i) > F.charAt(i)){
-                    System.out.print("-");
+                System.out.println();
+                System.out.println("Il vous reste encore " + essai + " tentatives");
+                m = sc.nextInt();
+                String F = str+nB;
+                String G = str+m;
+                //Integer.toString(m);
+                //System.out.println("Combinaison secrète : " + nB);
+                System.out.print("Votre proposition : " + m + " -> réponse : " );
+                for(i = 0; i < F.length(); i++){
+                    if (G.charAt(i) == F.charAt(i)){
+                        System.out.print("=");
+                    } else if (G.charAt(i) < F.charAt(i)){
+                        System.out.print("+");
+                    } else if (G.charAt(i) > F.charAt(i)){
+                        System.out.print("-");
+                    }
+
                 }
+                essai--;
 
-            }
 
-            //if
-        } while (m != nB);
-        System.out.println();
-        System.out.println("Bravo ! La combinaison secrète était bien " + nB);
+
+        } while (m != nB && essai != 0);
+        if (m == nB){
+            System.out.println();
+            System.out.println("Bravo ! La combinaison secrète était bien " + nB);
+        } else {
+            System.out.println("Malheureusement vous n'avez pas trouvé la bonne combinaison qui était :  "  + nB);
+
+        }
+
     }
 
 }
