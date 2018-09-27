e46cf6d0f0a4ceef38a29939fff247488fb84744
Alvindu13 <alcaraz.jeremie@hotmail.fr>
2018-09-27 15:03:53 +0200

    Mode défense plus ou moins ok
    Fonction PNP à prendre en compte

--- Game/src/main/java/com/company/Game.java
+++ Game/src/main/java/com/company/Game.java
@@ -14,15 +14,15 @@ public class Game {
     Game2Mastermind Game2Mastermind = new Game2Mastermind();
     int nbCases;
     int nbTry;
+    char[] formatColoursGame = new char[10];
     Scanner sc = new Scanner(System.in);
 
     /**
      * Affiche texte choix de la longueur du chiffre à trouver
      */
     public void numberRun() {
-
-        gameMode();
-        gameChoise();
+        int modeGame = gameMode();
+        gameChoise(modeGame);
     }
 
     /**
@@ -44,7 +44,7 @@ public class Game {
     /**
      * Select game and display it
      */
-    public void gameChoise() {
+    public void gameChoise(int gameMode) {
         int numberGame = 0;
         boolean whatGame;
         boolean numberChoiseIsGood;
@@ -78,50 +78,74 @@ public class Game {
                 case 2:
                     whatGame = true;
                     this.readParameters(whatGame);
-                    Game2Mastermind.availableColours();
-                    Game2Mastermind.challengeMode(nbCases);
+                    if (gameMode == 1)
+                        Game2Mastermind.challengeModeMastermind(nbCases, formatColoursGame);
+                    else if(gameMode == 2)
+                        Game2Mastermind.defenseModeMastermind(nbCases, formatColoursGame);
             }
         }
     }
 
-    public void gameMode() {
+    public int gameMode() {
         int choise;
         String[] arrayMode = {"1 - Mode Challenger : vous devez trouver la combinaison secrète de l'ordinateur", "2 - Mode Défenseur : où c'est à l'ordinateur de trouver votre combinaison secrète ", "3 - Mode duel : où l'ordinateur et vous jouez tour à tour, le premier à trouver la combinaison secrète de l'autre a gagné"};
-        System.out.println(String.format("Veuillez choisir le mode de jeu : %n"));
-        for(int index = 0; index < arrayMode.length; index++)
+        System.out.println("Veuillez choisir le mode de jeu :");
+        for (int index = 0; index < arrayMode.length; index++)
             System.out.println(arrayMode[index]);
         choise = sc.nextInt();
-    }
+        return choise;
+        }
 
-    public void readParameters(boolean choiseMastermind){
+
+    public void readParameters(boolean choiseMastermind) {
         String[] stockColoursAvailable = new String[10];
         try {
             Reader in = new FileReader("src/main/ressources/config.properties.csv");
             Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
-            String[] couloursAvailable = {"R-Rouge", "J-Jaune", "B-Bleu", "I-Indigo", "M-Marron", "V-Vert", "G-Gris", "N-Noir", "O-Orange", "P-Pourpre"};
+            String[] couloursAvailable = {"Rouge", "Jaune", "Bleu", "Indigo", "Marron", "Vert", "Gris", "Noir", "Orange", "Pourpre"};
             for (CSVRecord record : records) {
                 int nParseCases = Integer.valueOf(record.get("nombreCases"));
-                if(nParseCases > 0)
+                if (nParseCases > 0)
                     nbCases = nParseCases;
                 int nParsebTry = Integer.valueOf(record.get("essai"));
-                if(nParsebTry > 0)
+                if (nParsebTry > 0)
                     nbTry = nParsebTry;
                 int nColours = Integer.valueOf(record.get("couleurs"));
                 stockColoursAvailable[nColours] = couloursAvailable[nColours];
+            }
+            System.out.println(String.format("La taille des combinaisons est de  " + nbCases + " et vous avez le droit à " + nbTry + " essais%n"));
+            if (choiseMastermind == true) {
+                System.out.println("Les couleurs disponibles sont : ");
+                for (int index = 1; index <= stockColoursAvailable.length; index++) {
+                    if (index < stockColoursAvailable.length)
+                        System.out.print(stockColoursAvailable[index - 1] + ", ");
+                    else
+                        System.out.println(stockColoursAvailable[index - 1] + ".");
                 }
-            System.out.println(String.format("La taille des combinaisons est de  " + nbCases + " et vous avez le droit à " +nbTry+ " essais%n"));
-            if(choiseMastermind == true) {
-                System.out.println(String.format("Les couleurs disponibles sont : %n"));
-                for (int index = 1; index < stockColoursAvailable.length; index++)
-                    System.out.println(stockColoursAvailable[index - 1]);
+                System.out.println();
+                System.out.println("Pour le jeu, il faudra utiliser le format suivant pour proposer une combinaison : ");
+                for (int index = 1; index <= stockColoursAvailable.length; index++) {
+                    formatColoursGame[index - 1] = couloursAvailable[index - 1].charAt(0);
+                    if (index < formatColoursGame.length)
+                        System.out.print(formatColoursGame[index - 1] + ", ");
+                    else
+                        System.out.println(formatColoursGame[index - 1] + ".");
+                }
+                System.out.println();
+                System.out.println("Exemple d'une proposition valide : RJBJ équivaut à Rouge, Jaune, Bleu, Jaune \n");
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
+
+
 }
+
 
 
 
--- Game/src/main/java/com/company/Game2Mastermind.java
+++ Game/src/main/java/com/company/Game2Mastermind.java
@@ -1,62 +1,64 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game2Mastermind {
     int essai = 5;
+
     /**
      * Create a random array char with colours
-     * @param nB size of array
+     *
+     * @param nbCases size of array
      */
-    public void challengeMode(int nB) {
+    public void challengeModeMastermind(int nbCases, char[] format) {
         int s = 0;
         int indice = 0;
         int counter = 0;
-        char[] colours1 = {'R', 'J', 'V', 'B', 'N', 'G'};
-        char[] randomColours = new char[nB];
-        char[] rrandomColours = new char[nB];
-        char[] responseS = new char[nB];
-        char[] stock = new char[nB];
-        System.out.println("La combinaison secrète est : ");
-        for (int k = 0; k < nB; k++) {
+        char[] randomColours = new char[nbCases];
+        char[] stockRandomColours = new char[nbCases];
+        char[] responseToChar = new char[nbCases];
+        char[] stockResponseToChar = new char[nbCases];
+        for (int indexColour = 0; indexColour < nbCases; indexColour++) {
             int bMin = 0;
-            int bMax = 5;
-            int nomb = (int) (Math.random() * (bMax - bMin)) + bMin;
-            randomColours[k] = colours1[nomb];
-            System.out.print(randomColours[k]);
+            int bMax = 10;
+            int numRandom = (int) (Math.random() * (bMax - bMin)) + bMin;
+            randomColours[indexColour] = format[numRandom];
+            System.out.print(randomColours[indexColour]);
         }
-        for (int u = 0; u < nB; u++)
-            rrandomColours[u] = randomColours[u];
-        for (int om = 0; om < 5; om++) {
-            char[] testA = {'0', '1', '2', '3', '4'};
-            char[] testB = {'5', '6', '7', '8', '9'};
+        for (int index = 0; index < nbCases; index++)
+            stockRandomColours[index] = randomColours[index];
+        for (int in = 0; in < 5; in++) {
+            char[] testA = {'0', '1', '2', '3'};
+            char[] testB = {'5', '6', '7', '8'};
             System.out.println();
             System.out.println("Il vous reste encore " + essai + " tentatives");
             Scanner nk = new Scanner(System.in);
-            String response = nk.nextLine();
-            for (int jk = 0; jk < nB; jk++) {
-                responseS[jk] = response.charAt(jk);
-                randomColours[jk] = rrandomColours[jk];
-                stock[jk] = responseS[jk];
+            String responseString = nk.nextLine();
+            for (int indexRep = 0; indexRep < nbCases; indexRep++) {
+                responseToChar[indexRep] = responseString.charAt(indexRep);
+                randomColours[indexRep] = stockRandomColours[indexRep];
+                stockResponseToChar[indexRep] = responseToChar[indexRep];
             }
-            while (response.length() != nB) {
+            while (responseString.length() != nbCases) {
                 System.out.println("Merci de faire une proposition valide : ");
-                challengeMode(nB);
+                challengeModeMastermind(nbCases, format);
             }
-            for (int i = 0; i < nB; i++) {
-                for (int o = 0; o < nB; o++) {
-                    if (randomColours[o] == responseS[i] && o == i) {
-                        counter++;
-                        s++;
-                        responseS[i] = testA[i];
-                        randomColours[o] = testB[o];
-                    }
+
+            for (int i = 0; i < nbCases; i++) { //Je pense qu'un des for est useless, à voir
+                //for (int o = 0; o < nbCases; o++) {
+                if (randomColours[i] == responseToChar[i] && i == i) {
+                    counter++;
+                    s++;
+                    responseToChar[i] = testA[i];
+                    randomColours[i] = testB[i];
                 }
+                //}
             }
-            for (int q = 0; q < nB; q++) {
-                for (int r = 0; r < nB; r++) {
-                    if (randomColours[r] == responseS[q]) {
-                        responseS[q] = testA[q];
+            for (int q = 0; q < nbCases; q++) {
+                for (int r = 0; r < nbCases; r++) {
+                    if (randomColours[r] == responseToChar[q]) {
+                        responseToChar[q] = testA[q];
                         randomColours[r] = testB[r];
                         s++;
                     }
@@ -72,44 +74,229 @@ public class Game2Mastermind {
             counter = 0;
             s = 0;
             essai--;
-            for (indice = 0; indice < stock.length && stock[indice] == rrandomColours[indice]; indice++);
+            for (indice = 0; indice < stockResponseToChar.length && stockResponseToChar[indice] == stockRandomColours[indice]; indice++)
+                ;
 
-            if (indice == nB){
+            if (indice == nbCases) {
                 System.out.println();
-                System.out.println("Bravo vous avez trouver la bonne combinaison. C'était bien : ");
-                for (int rep = 0; rep < nB; rep++) {
-                    System.out.print(rrandomColours[rep]);
+                System.out.print("Bravo vous avez trouver la bonne combinaison. C'était bien : ");
+                for (int rep = 0; rep < nbCases; rep++) {
+                    System.out.print(stockRandomColours[rep]);
                 }
                 break;
             }
         }
-        if(indice != nB){
+        if (indice != nbCases) {
+            System.out.println();
             System.out.print("Malheureusement, vous n'avez pas trouvé la réponse. La réponse était  : ");
-            for (int rep = 0; rep < nB; rep++)
-                System.out.print(rrandomColours[rep]);
+            for (int rep = 0; rep < nbCases; rep++)
+                System.out.print(stockRandomColours[rep]);
         }
     }
 
-    /**
-     * Display available colours
-     */
-    public void availableColours() {
-        String[] colours = {"R - Rouge", "J - Jaune", "V - Vert", "B - Bleu", "N - Noir", "G - Gris"};
-        System.out.println("Pour prendre connaissance des couleurs disponibles avant de jouer, merci d'entrer V dans le terminal : ");
+
+    public void defenseModeMastermind(int nbCases, char[] format) {
+
+        int goodPlace = 0;
+        int counter = 1;
+        int PNP = 0;
+        int indice = 0;
+        int i0 = 1;
+        int i1 = 0;
+        int i2 = 0;
+        int i3 = 0;
+        boolean j0 = false;
+        boolean j1 = false;
+        boolean j2 = false;
+        boolean j3 = false;
         Scanner sc = new Scanner(System.in);
-        String response = sc.nextLine();
-        if ("V".contains(Character.toString(response.charAt(0)))) {
-            System.out.println("Voici les couleurs disponibles : ");
-            for (int i = 1; i <= 6; i++)
-                System.out.println(colours[i - 1]);
-        } else {
-            System.out.println("Je ne comprends pas votre réponse, merci de rentrer une valeur valide");
-            availableColours();
+        int present = 0;
+        char[] stockGoodResponse = new char[nbCases];
+        for (int index = 0; index < nbCases; index++)
+            stockGoodResponse[index] = '0';
+        char[] yourColours = new char[nbCases];
+        char[] stockRandomColours = new char[nbCases];
+        char[] nextResponse = new char[nbCases];
+        char[] stockResponseRandom = new char[nbCases];
+        char[] responseRandomPC = new char[nbCases];
+        System.out.println("Veuillez rentrer la combinaison que l'ordinateur doit trouver ; ");
+        String yourResponse = sc.nextLine();
+        System.out.print("La combinaison secrète est : ");
+        System.out.println(yourResponse);
+        System.out.println();
+        responseRandomPC = randomColours(nbCases, format);
+        for (int index = 0; index < nbCases; index++)
+            yourColours[index] = yourResponse.charAt(index);
+        System.out.println(Arrays.toString(yourColours));
+
+
+        for (int in = 0; in < 5; in++) {
+            System.out.println();
+            System.out.println("Il vous reste encore " + essai + " tentatives");
+            char[] Sup1 = {'0', '1', '2', '3'};
+            char[] Sup2 = {'5', '6', '7', '8'};
+            for (int index = 0; index < nbCases; index++) { // stock la combinaison secrète
+                stockRandomColours[index] = yourColours[index];
+                stockResponseRandom[index] = responseRandomPC[index];
+            }
+            System.out.println("La combinaison secrète est : " + Arrays.toString(yourColours));
+            for (int i = 0; i < 4; i++) {
+                //for (int j = 0; j < nbCases; j++) {
+                if (responseRandomPC[i] == yourColours[i]) {
+                    stockGoodResponse[i] = yourColours[i]; // stock les doublons
+                    responseRandomPC[i] = Sup1[i];
+                    yourColours[i] = Sup2[i];
+                    if (i == 0){
+                        j0 = true;
+                    }
+                    if (i == 1){
+                        j1 = true;
+                    }
+                    if (i == 2){
+                        j2 = true;
+                    }
+                    if (i == 3){
+                        j3 = true;
+                    }
+                    /**switch (i) {
+                        case 0:
+                            j0 = true;
+                            i0 = 0;
+                            break;
+                        case 1:
+                            j1 = true;
+                            i1 = 1;
+                            break;
+                        case 2:
+                            j2 = true;
+                            i2 = 2;
+                            break;
+                        case 3:
+                            j3 = true;
+                            i3 = 3;
+                            break;
+                    */
+
+                }
+
+                if(j0 == true)
+                    i0 = 0;
+                if(j1 == true)
+                    i1 = 1;
+                if(j2 == true)
+                    i2 = 2;
+                if(j3 == true)
+                    i3 = 3;
+
+            }
+            //for (int index = 1; index <= nbCases; index++)
+            //    System.out.println(stockGoodResponse[index - 1]);
+
+            for (int q = 0; q < nbCases; q++) {
+                for (int r = 0; r < nbCases; r++) {
+                    if (yourColours[r] == responseRandomPC[q]) {
+                        responseRandomPC[q] = Sup1[q];
+                        yourColours[r] = Sup2[r];
+                        PNP++;
+                    }
+                }
+            }
+            //System.out.print("i et j : " + i0 + ", " + i1 + ", " + i2 + ", " + i3 + ", " + j0 + ", " + j1 + ", " + j2 + ", " + j3);
+            //if (PNP > 0) {
+                if (essai == 5) {
+                    responseRandomPC = stockResponseRandom;
+                    System.out.println();
+                    System.out.print("réponse du PC " + counter + " : ");
+                    System.out.println(Arrays.toString(responseRandomPC));
+                    responseRandomPC = randomColours(nbCases, format);
+                    if (i0 == 0)
+                        responseRandomPC[i0] = stockGoodResponse[i0];
+                    if (i1 == 1)
+                        responseRandomPC[i1] = stockGoodResponse[i1];
+                    if (i2 == 2)
+                        responseRandomPC[i2] = stockGoodResponse[i2];
+                    if (i3 == 3)
+                        responseRandomPC[i3] = stockGoodResponse[i3];
+                }
+
+
+                if (essai >= 1 && essai < 5) {
+                    responseRandomPC = stockResponseRandom;
+                    System.out.println();
+                    System.out.print("réponse du PC " + counter + " : ");
+                    System.out.println(Arrays.toString(responseRandomPC));
+                    responseRandomPC = randomColours(nbCases, format);
+                    if (i0 == 0)
+                        responseRandomPC[i0] = stockGoodResponse[i0];
+                    if (i1 == 1)
+                        responseRandomPC[i1] = stockGoodResponse[i1];
+                    if (i2 == 2)
+                        responseRandomPC[i2] = stockGoodResponse[i2];
+                    if (i3 == 3)
+                        responseRandomPC[i3] = stockGoodResponse[i3];
+                }
+           /** } else {
+                System.out.println("Réponse du pc else : ");
+                responseRandomPC = randomColours(nbCases, format);
+                for (int count = 0; count < nbCases; count++)
+                    stockResponseRandom = responseRandomPC;
+                if (j0 == true) {
+                    responseRandomPC[i0] = stockGoodResponse[i0];
+                } else if (j1 == true) {
+                    responseRandomPC[i1] = stockGoodResponse[i1];
+                } else if (j2 == true) {
+                    responseRandomPC[i2] = stockGoodResponse[i2];
+                } else if (j3 == true) {
+                    responseRandomPC[i3] = stockGoodResponse[i3];
+                }
+                System.out.println("valeur de response indice i0 : " + responseRandomPC[i0]);
+                System.out.println("valeur de response indice i1 : " + responseRandomPC[i1]);
+                System.out.println("valeur de response indice i2 : " + responseRandomPC[i2]);
+                System.out.println("valeur de response indice i3 : " + responseRandomPC[i3]);
+
+            }**/
+
+            for (int index = 0; index < nbCases; index++)
+                yourColours[index] = stockRandomColours[index];
+            System.out.println("PNP = " + PNP +"\n");
+            essai--;
+            counter++;
+            PNP = 0;
+
         }
+
+
+
+            //nextresponse = stockGoodResponse[index - ceux qui sont bons}
+            System.out.println("présent " + present);
+            System.out.println("Bien placé" + goodPlace);
+            int find = 0;
+            for (int index = 1; index <= nbCases; index++) {
+                if (stockGoodResponse[index - 1] != '0')
+                    find++;
+            }
+
+        System.out.println("Vous avez trouvé " + find + " élément(s) de la combinaison");
+
+
+            for (int index = 1; index <= nbCases; index++)
+                System.out.println(stockGoodResponse[index - 1]);
+            //System.out.println(Arrays.toString(stockGoodResponse));
+
+
+
     }
 
-    public static void defenseMode(){
+    public char[] randomColours(int nbCases, char[] format) {
+
+        char[] responseRandomPC = new char[nbCases];
+        for (int indexColour = 0; indexColour < nbCases; indexColour++) { //génère une série de 4 couleurs aléatoire pour la réponse de l'ordi
+            int bMin = 0;
+            int bMax = 10;
+            int numRandom = (int) (Math.random() * (bMax - bMin)) + bMin;
+            responseRandomPC[indexColour] = format[numRandom];
+        }
+        return responseRandomPC;
 
     }
 }
-
